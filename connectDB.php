c89110c9502c0f2153c919d8f976db5a32fda5f2
RychardProficional <rychardproficional@gmail.com>
2023-02-26 20:31:26 -0400

    restart

--- connectDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-const hostname = "localhost";
-const username = "root";
-const password = "";
-const database = "users";
-$db = new mysqli(hostname, username, password, database) or die("Não foi possivel conectar-se ao banco de dados: ".$db->connect_error);
--- database.sql
+++
@@ -1,9 +0,0 @@
-  CREATE DATABASE users;
-USE users;
-
-CREATE TABLE `user`(
-    `id` int(6) UNSIGNED ZEROFILL AUTO_INCREMENT NOT NULL,
-    `username` varchar(30) NOT NULL,
-    `password` varchar(30) NOT NULL,
-    PRIMARY KEY(`id`)
-)
